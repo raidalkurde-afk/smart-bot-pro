f0bf0ae4fd3e0e581650f8d3471dffa779c5f33e
raidalkurde-afk <raidalkurde@gmail.com>
2026-03-26 21:28:49 +0300

    Add prediction handler based on price input

--- api/predict.js
+++ api/predict.js
@@ -1,1 +1,11 @@
+export default function handler(req, res) {
+  const { price } = req.body || {};
 
+  let prediction = 0;
+
+  if (price && price > 1000) {
+    prediction = 1;
+  }
+
+  res.status(200).json({ prediction });
+}
